e843c0d3156d94e23ce39dca9a5ced2938c007b0
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2021-01-28 09:15:38 +0300

    Checking

--- Traffic lights/ViewController.swift
+++ Traffic lights/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightofTrafficView: UIView!
+    @IBOutlet var yellowLightOfTrafficView: UIView!
+    @IBOutlet var greenLightOfTrafficView: UIView!
+    
+    @IBOutlet var changeLightsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightofTrafficView.alpha = 0.3
+        redLightofTrafficView.layer.cornerRadius = 50
+        
+        yellowLightOfTrafficView.alpha = 0.3
+        yellowLightOfTrafficView.layer.cornerRadius = 50
+        
+        greenLightOfTrafficView.alpha = 0.3
+        greenLightOfTrafficView.layer.cornerRadius = 50
     }
 
 
-}
 
+    @IBAction func startButtonPassed() {
+        if redLightofTrafficView.alpha != 0 {
+            redLightofTrafficView.alpha = 1
+            changeLightsButton.setTitle("NEXT", for: .normal)
+        }
+        else if yellowLightOfTrafficView.alpha != 0 {
+            redLightofTrafficView.alpha = 0.3
+            yellowLightOfTrafficView.alpha = 1
+            changeLightsButton.setTitle("NEXT", for: .normal)
+        }
+        else if greenLightOfTrafficView.alpha != 0 {
+            yellowLightOfTrafficView.alpha = 0.3
+            greenLightOfTrafficView.alpha = 1
+            changeLightsButton.setTitle("NEXT", for: .normal)
+        }
+    }
+}
